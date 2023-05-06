a2a61ee5a48a44f9d86b8e8174aa746ab896f30a
yarshy38 <muhammadyaseen62545@gmail.com>
2023-05-06 13:56:20 +0500

    added logo to templeta and hero image and  about us image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import './index.css';
 import React from "react";
 import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 import Menu from "./components/Navbar/Home";
@@ -16,14 +18,11 @@ function App() {
           <Menu />
           <Routes>
             <Route path="/" element={<Home />} />
-          </Routes>
-          <Routes>
+
             <Route path="/contact" element={<Contact />} />
-          </Routes>
-          <Routes>
+
             <Route path="/service" element={<Service />} />
-          </Routes>
-          <Routes>
+
             <Route path="/about" element={<About />} />
           </Routes>
         </div>
--- src/components/Navbar/Home.js
+++ src/components/Navbar/Home.js
@@ -7,21 +7,30 @@ import Navbar from 'react-bootstrap/Navbar';
 const Menu = () => {
     return (
         <>
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand to="/">React_Temp</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mx-auto">
-                            <Nav.Link to="/">Home</Nav.Link>
-                            <Nav.Link to="/service">Service</Nav.Link>
-                            <Nav.Link to="/about">About</Nav.Link>
-                            <Nav.Link to="/contact">Contact</Nav.Link>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-10'>
+                        <Navbar bg="" expand="lg">
+                            <Container>
+                                <Navbar.Brand to="/">
+                                    <img src='./assets/img/logo.png' alt='logo' className='img-fluid for-logo' />
+                                </Navbar.Brand>
+                                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                                <Navbar.Collapse id="basic-navbar-nav">
+                                    <Nav className="mx-auto">
+                                        <Nav.Link href="/">Home</Nav.Link>
+                                        <Nav.Link href="/service">Service</Nav.Link>
+                                        <Nav.Link href="/about">About</Nav.Link>
+                                        <Nav.Link href="/contact">Contact</Nav.Link>
 
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+                                    </Nav>
+                                </Navbar.Collapse>
+                            </Container>
+                        </Navbar>
+                    </div>
+                </div>
+            </div >
+
         </>
     )
 }
