19dab2ef86ec0cf894548332870d3d065db1da7b
yarshy38 <muhammadyaseen62545@gmail.com>
2023-06-05 12:56:20 +0500

    final product

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 const About = () => {
     return (
@@ -12,7 +13,7 @@ const About = () => {
                                     <h2 className='fw-bold  fs-1'>Welcome to About us <br />
                                         <span className='span-text'>React Template</span></h2>
                                     <p className='text-muted'>It's that amazing content that is significant to making inbound marketing work for your business. </p>
-                                    <button className='btn btn-primary'>Contact Now</button>
+                                    <NavLink className='btn btn-primary' to='/contact'>Contact Now</NavLink>
                                 </div>
                                 <div className='col-lg-4'>
                                     <img src='./assets/img/boyUsingLaptop.png' alt='about us' className='img-fluid' />
